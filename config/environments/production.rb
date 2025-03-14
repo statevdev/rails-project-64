3e49210c4b969561cb47ea2b99f8d8bcde651e64
statevdev <statev.dev@gmail.com>
2025-03-14 06:12:09 +0600

    setting Devise, added navigation for authorization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'rails-project-64-o0g0.onrender.com', protocol: 'https' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
