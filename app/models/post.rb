1f8f09045c51a3bb41e1507ef548991841139a8a
statevdev <statev.dev@gmail.com>
2025-03-23 08:39:35 +0600

    fix relations in model Category

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: 'User', inverse_of: :posts
-  belongs_to :category
+  belongs_to :category, dependent: :destroy
 
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
