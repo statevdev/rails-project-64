30f4b7f6a99a561f68ff830a232d826edda55475
statevdev <statev.dev@gmail.com>
2025-03-23 09:07:41 +0600

    deploy trying after delete prod database

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ setup:
 	bundle
 	bin/rails assets:precompile
 	bin/rails db:migrate
-	DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:drop db:create db:migrate db:fixtures:load
+	bin/rails db:fixtures:load
 start:
 	bin/rails s
 lint:
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-categories = %w[Music Cinema IT]
 
-categories.each do |category|
-  Category.create(name: category)
-end
