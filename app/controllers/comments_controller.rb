52116653fa7923291c731954ff414926570cc643
statevdev <statev.dev@gmail.com>
2025-03-25 08:04:05 +0600

    added translations to locale file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     @comment.parent_id = params[:parent_id] if params[:parent_id].present?
 
-    flash[:success] = t('comments.created') if @comment.save
+    flash[:notice] = t('comments.created') if @comment.save
 
     redirect_to @post
   end
