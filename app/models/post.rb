b7e18bf7e3baf3d76bc3f3bc514cc15bbe17b622
statevdev <statev.dev@gmail.com>
2025-03-25 08:20:00 +0600

    remove redundant validates present: true

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
 
-  validates :category_id, presence: true
   validates :title, presence: true
   validates :body, presence: true
 end
